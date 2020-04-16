093e9c58c243c192b9010a2108c648ce99483d8d
Sumit Kumar <39203444+sumit-kishore@users.noreply.github.com>
2020-04-16 17:14:24 +0530

    Update Setting.py

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -133,8 +133,8 @@ LOGIN_URL = 'login'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Sumit@1998' #my gmail password
-EMAIL_HOST_USER = 'sumitkishore0131@gmail.com' #my gmail username
+EMAIL_HOST_PASSWORD = '********' #my gmail password
+EMAIL_HOST_USER = '***********' #my gmail username
 EMAIL_PORT = 587
 
 
